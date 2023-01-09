775f011cb6e1be01bf17558c2f6bb3c979cd7da2
Nicholas <nicholas.harviel@iointegration.com>
2023-01-09 16:15:47 -0600

    updated logic per feedback

--- backend/models/User.js
+++ backend/models/User.js
@@ -88,7 +88,7 @@ UserSchema.methods.toProfileJSONFor = function(user) {
     image:
       this.image || "https://static.productionready.io/images/smiley-cyrus.jpg",
     following: user ? user.isFollowing(this._id) : false,
-    isVerified: user.isVerified !== undefined ? user.isVerified || false : false
+    isVerified: user.isVerified !== undefined ? user.isVerified : false
   };
 };
 
